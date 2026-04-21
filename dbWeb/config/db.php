63d94228cf9c12f1d6bdee7364307e827fef3ec2
Anthony <talisikninoanthony@gmail.com>
2026-04-21 21:34:13 +0800

    changes on database password and name

--- dbWeb/config/db.php
+++ dbWeb/config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "imdbsys31", "WorkSphere");
+$conn = new mysqli("localhost", "root", "", "LandersOnline");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
